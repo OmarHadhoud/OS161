667607001e8b3777acaf40cde8b911fc4133b100
OmarHadhoud <omarhadhoud@ymail.com>
2020-06-20 19:57:03 +0000

    Fixed the stoplight problem

--- kern/synchprobs/stoplight.c
+++ kern/synchprobs/stoplight.c
@@ -70,11 +70,60 @@
 #include <synch.h>
 
 /*
+ * Locks and semaphores needed for the solution
+ */
+struct semaphore *intersection_sem = NULL;
+struct lock *quad0 = NULL;
+struct lock *quad1 = NULL;
+struct lock *quad2 = NULL;
+struct lock *quad3 = NULL;
+struct lock *quads[4];
+
+/*
  * Called by the driver during initialization.
  */
 
 void
 stoplight_init() {
+	
+	intersection_sem = sem_create("Intersection semaphore",3);
+	if (intersection_sem == NULL) {
+		panic("Couldn't create the intersection sem\n");
+	}
+
+	quad0 = lock_create("Quad 0 lock");
+	if (quad0 == NULL) {
+		kfree(intersection_sem);
+		panic("Couldn't create the quad 0 lock");
+	}
+
+	quad1 = lock_create("Quad 1 lock");
+	if (quad1 == NULL) {
+		kfree(intersection_sem);
+		kfree(quad0);
+		panic("Couldn't create the quad 1 lock");
+	}
+
+	quad2 = lock_create("Quad 2 lock");
+	if (quad2 == NULL) {
+		kfree(intersection_sem);
+		kfree(quad0);
+		kfree(quad1);
+		panic("Couldn't create the quad 2 lock");
+	}
+
+	quad3 = lock_create("Quad 3 lock");
+	if (quad3 == NULL) {
+		kfree(intersection_sem);
+		kfree(quad0);
+		kfree(quad1);
+		kfree(quad2);
+		panic("Couldn't create the quad 3 lock");
+	}
+	quads[0] = quad0;
+	quads[1] = quad1;
+	quads[2] = quad2;
+	quads[3] = quad3;
 	return;
 }
 
@@ -83,36 +132,75 @@ stoplight_init() {
  */
 
 void stoplight_cleanup() {
+	sem_destroy(intersection_sem);
+	lock_destroy(quad0);
+	lock_destroy(quad1);
+	lock_destroy(quad2);
+	lock_destroy(quad3);
 	return;
 }
 
 void
 turnright(uint32_t direction, uint32_t index)
 {
-	(void)direction;
-	(void)index;
-	/*
-	 * Implement this function.
-	 */
+	//Make sure there's at most 2 cars before entering intersection
+	P(intersection_sem);
+	//Acquire the quad we're going to if it is empty and update it in driver
+	lock_acquire(quads[direction]);
+	inQuadrant(direction, index);
+	//Leave the inresection as we turn right, after turning right we leave the lock for the quad for other cars.
+	leaveIntersection(index);
+	lock_release(quads[direction]);
+	//Increment the semaphore to indicate a car has left the intersection
+	V(intersection_sem);
 	return;
 }
 void
 gostraight(uint32_t direction, uint32_t index)
 {
-	(void)direction;
-	(void)index;
-	/*
-	 * Implement this function.
-	 */
+	uint32_t passing_direction = (direction + 3) % 4;
+	//Make sure there's at most 2 cars before entering intersection
+	P(intersection_sem);
+	//Acquire the quad we're going to if is is empty, update it in the driver.
+	lock_acquire(quads[direction]);
+	inQuadrant(direction, index);
+	//Request to acquire next quad if it is empty, if not, we wait until it is empty.
+	lock_acquire(quads[passing_direction]);
+	//Update the quad.
+	inQuadrant(passing_direction, index);
+	//Release the lock of previous quad for other cars as we moved.
+	lock_release(quads[direction]);
+	//Leave the intersection and release the lock for other cars.
+	leaveIntersection(index);
+	lock_release(quads[passing_direction]);
+	//Increment the semaphore to indicate a car has left the intersection
+	V(intersection_sem);
 	return;
 }
 void
 turnleft(uint32_t direction, uint32_t index)
 {
-	(void)direction;
-	(void)index;
-	/*
-	 * Implement this function.
-	 */
+	uint32_t passing_direction = (direction + 3) % 4;
+	uint32_t passing_direction2 = (direction + 2) % 4;
+	//Make sure there's at most 2 cars before entering intersection
+	P(intersection_sem);
+	//Acquire the quad we're going to if is is empty, update it in the driver.
+	lock_acquire(quads[direction]);
+	inQuadrant(direction, index);
+	lock_acquire(quads[passing_direction]);
+	//Update the quad.
+	inQuadrant(passing_direction, index);
+	//Release the lock of previous quad for other cars as we moved.
+	lock_release(quads[direction]);
+	lock_acquire(quads[passing_direction2]);
+	//Update the quad.
+	inQuadrant(passing_direction2, index);
+	//Release the lock of previous quad for other cars as we moved.
+	lock_release(quads[passing_direction]);
+	//Leave the intersection and release the lock for other cars.
+	leaveIntersection(index);
+	lock_release(quads[passing_direction2]);
+	//Increment the semaphore to indicate a car has left the intersection
+	V(intersection_sem);
 	return;
 }
